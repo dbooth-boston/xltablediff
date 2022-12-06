37427d78c5978babb1a34fb863aacab5a0083c91
David Booth <david@dbooth.org>
2022-12-05 20:58:24 -0500

    Improved help and tests

---
+++ runtests.bash
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Diff test:
+  xltablediff.py  --key ID test1old.xlsx test1new.xlsx --out test1diff.xlsx
+
+# Ignore test:
+  xltablediff.py  --key ID --ignore Color test1old.xlsx test1new.xlsx --out test1ignore.xlsx
+
+# Merge test:
+  xltablediff.py  --key ID --merge Color test1old.xlsx test1new.xlsx --out test1merge.xlsx
+
+# Append test:
+  xltablediff.py  --key ID --append test1old.xlsx test1new.xlsx --out test1append.xlsx
+
+
+
